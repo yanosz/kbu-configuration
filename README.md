a4d50d1f5f386f8f0063901e1c659efb272a8b0f
root <root@Daisy>
2015-04-04 22:04:35 +0200

    Fix ImageBuilder SDK naming confusion

--- README.md
+++ README.md
@@ -2,6 +2,6 @@
 Image-Builder configuration for Freifunk-KBU using OpenWRT 14.07
 
 ## Usage
-1. Extract the OpenWRT-SDK version 14.07 to <code>./sdk</code> according to your platform
+1. Extract the OpenWRT-Image Builder version 14.07 to <code>./build</code> according to your platform
 2. Run <code>./build.sh PROFILE</code> with PROFILE specifying the ImageBuilder profile to be used.
  
--- build.sh
+++ build.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 if [ ! -d "sdk" ]; then
-  echo "Missing OpenWRT-SDK in ./sdk"
+  echo "Missing OpenWRT-Image Builder in ./build"
 fi
 
 # Link repositories
-rm -f sdk/repositories.conf
-ln -s ../repositories.conf sdk/repositories.conf
+rm -f build/repositories.conf
+ln -s ../repositories.conf build/repositories.conf
 
 # Link files
-rm -rf sdk/files
-ln -s ../files sdk/files
+rm -rf build/files
+ln -s ../files build/files
 
 # Call image-builder using the supplied profile
-make -C sdk/ image PACKAGES="`cat packages`" FILES=./files PROFILE=$1
+make V=99 -C build/ image PACKAGES="`cat packages`" FILES=./files PROFILE=$1 CONFIG_VERSION_NUMBER="KBU-2.x" 
