b68cce92eff6d9999caa1e4fba98a07d3aa3916d
yanosz <yanosz@gmx.net>
2015-04-21 15:48:56 +0200

    Fix missing whitespace

--- build.sh
+++ build.sh
@@ -12,7 +12,7 @@ rm -rf build/files
 ln -s ../files build/files
 
 # Call image-builder using the supplied profile
-if [ ! -z $1]; then
+if [ ! -z "$1" ]; then
 	make V=99 -C build/ image PACKAGES="`cat packages`" FILES=./files PROFILE=$1
 else
 	make V=99 -C build/ image PACKAGES="`cat packages`" FILES=./files
